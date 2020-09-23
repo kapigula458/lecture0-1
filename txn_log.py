84093afb0a0c32d2b2f679606284e0b533179190
kapigula458 <66946144+kapigula458@users.noreply.github.com>
2020-09-23 19:28:18 +0530

    Add files via upload

---
+++ txn_log.py
@@ -0,0 +1,30 @@
+'''
+Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+D 100
+W 200
+
+D means deposit while W means withdrawal.
+Suppose the following input is supplied to the program:
+D 300
+D 300
+W 200
+D 100
+Then, the output should be:
+500
+'''
+bal=0
+while True:
+	j=input()
+	if not j:
+		break;
+
+	list1=j.split(" ")
+	if list1[0]=="D":
+		bal+=int(list1[1])
+	if list1[0]=="W":
+		bal-=int(list1[1])
+
+
+print(bal)
+
+
